e367aba101774444394db7fc42c6225e3417f95f
Edward Loveall <edward@edwardloveall.com>
2015-06-29 13:06:10 -0400

    Unicorn configuration
    Found here: https://www.digitalocean.com/community/tutorials/how-to-deploy-rails-apps-using-unicorn-and-nginx-on-centos-6-5#unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,30 +1,20 @@
-# https://devcenter.heroku.com/articles/rails-unicorn
+app_path = ENV.fetch('PULLFEED_PATH')
 
-worker_processes (ENV["UNICORN_WORKERS"] || 3).to_i
-timeout (ENV["UNICORN_TIMEOUT"] || 15).to_i
-preload_app true
+# Set the working application directory
+working_directory app_path
 
-before_fork do |_server, _worker|
-  Signal.trap "TERM" do
-    puts "Unicorn master intercepting TERM, sending myself QUIT instead"
-    Process.kill "QUIT", Process.pid
-  end
+# Unicorn PID file location
+pid "#{app_path}/pids/unicorn.pid"
 
-  if defined? ActiveRecord::Base
-    ActiveRecord::Base.connection.disconnect!
-  end
-end
+# Path to logs
+stderr_path "#{app_path}/log/unicorn.log"
+stdout_path "#{app_path}/log/unicorn.log"
 
-after_fork do |_server, _worker|
-  Signal.trap "TERM" do
-    puts "Unicorn worker intercepting TERM, waiting for master to send QUIT"
-  end
+# Unicorn socket
+listen "#{app_path}/sockets/unicorn.pullfeed.sock"
 
-  if defined? ActiveRecord::Base
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-      Rails.application.config.database_configuration[Rails.env]
-    config["reaping_frequency"] = (ENV["DB_REAPING_FREQUENCY"] || 10).to_i
-    config["pool"] = (ENV["DB_POOL"] || 2).to_i
-    ActiveRecord::Base.establish_connection(config)
-  end
-end
+# Number of processes
+worker_processes 2
+
+# Time-out
+timeout 30
