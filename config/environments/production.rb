de2831a01ab6f1d033121cabc6b2cf857bcd965a
Edward Loveall <edward@edwardloveall.com>
2015-07-05 14:43:04 -0400

    Remove activerecord

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,5 +19,4 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 end
 
-ActiveRecord::Migration.maintain_test_schema!
 Capybara.javascript_driver = :webkit
