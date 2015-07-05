f0ad114fe873c08f5c88921d954b2d2097eb5d60
Edward Loveall <edward@edwardloveall.com>
2015-07-05 14:16:14 -0400

    Rename rails app to pullfeed

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PrRss
+module PullFeed
   class Application < Rails::Application
     config.i18n.enforce_available_locales = true
 
