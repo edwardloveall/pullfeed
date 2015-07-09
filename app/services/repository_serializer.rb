ca7b1478a23c3191e4ef1fdb9a3d87f72b88f459
Edward Loveall <edward@edwardloveall.com>
2015-07-09 14:18:42 -0400

    RepositorySerializer can now handle absense of PRs
    Before, it was assumed that PR data was being passed into the RepositorySerializer. But if there are no PRs, an empty array is all that will be passed. It now intelligently handles raw repo data.
    
    This will be helpful in the next few commits when I fetch repo data instead.

--- app/services/repository_serializer.rb
+++ app/services/repository_serializer.rb
@@ -15,28 +15,44 @@ class RepositorySerializer
 
   attr_reader :data
 
+  def parsed_data
+    if data.is_a?(Array)
+      { repository: data.first['base']['repo'], pull_requests: data }
+    else
+      { repository: data, pull_requests: [] }
+    end
+  end
+
+  def repository_data
+    @_repository_data = parsed_data[:repository]
+  end
+
+  def pull_requests_data
+    @_pull_requests_data = parsed_data[:pull_requests]
+  end
+
   def repository_attributes
     {
       created_at: created_at,
-      description: base_attributes['description'],
-      link: base_attributes['html_url'],
-      owner: base_attributes['owner']['login'],
+      description: repository_data['description'],
+      link: repository_data['html_url'],
+      owner: repository_data['owner']['login'],
       pull_requests: pull_requests,
-      title: base_attributes['name']
+      title: repository_data['name']
     }
   end
 
-  def base_attributes
-    @_base_attributes ||= data.first['base']['repo']
-  end
-
   def pull_requests
-    @data.map do |pull_request_data|
-      PullRequestSerializer.perform(pull_request_data)
+    pull_requests_data.map do |pull_request|
+      PullRequestSerializer.perform(pull_request)
     end
   end
 
   def created_at
-    DateTime.parse(data.first['created_at'])
+    if !pull_requests_data.empty?
+      DateTime.parse(pull_requests_data.first['created_at'])
+    else
+      DateTime.parse(repository_data['created_at'])
+    end
   end
 end
--- spec/services/repository_serializer_spec.rb
+++ spec/services/repository_serializer_spec.rb
@@ -2,34 +2,64 @@ require 'rails_helper'
 
 describe RepositorySerializer do
   describe '.perform' do
-    it 'returns a repository' do
-      repository = RepositorySerializer.perform(data)
+    context 'if passing in pull request data' do
+      it 'returns a repository' do
+        repository = RepositorySerializer.perform(pulls)
 
-      expect(repository).to be_a(Repository)
+        expect(repository).to be_a(Repository)
+      end
+
+      it 'fills the repository with attributes' do
+        repository = RepositorySerializer.perform(pulls)
+
+        expect(repository.created_at).to eq(Time.parse('2015-05-05T07:50:40Z'))
+        expect(repository.description).to eq('A repo with some really good code.')
+        expect(repository.link).to eq('https://github.com/github/code')
+        expect(repository.owner).to eq('github')
+        expect(repository.title).to eq('code')
+      end
+
+      it 'serializes pull requests' do
+        pull_request = pulls.first
+        allow(PullRequestSerializer).to receive(:perform).and_call_original
+
+        repository = RepositorySerializer.perform(pulls)
+
+        expect(PullRequestSerializer).to have_received(:perform).with(pull_request)
+        expect(repository.pull_requests.first).to be_a(PullRequest)
+      end
     end
 
-    it 'fills the repository with attributes' do
-      repository = RepositorySerializer.perform(data)
+    context 'if passing in only repo data' do
+      it 'returns a repository' do
+        repository = RepositorySerializer.perform(repository_data)
 
-      expect(repository.created_at).to eq(Time.parse('2015-05-05T07:50:40Z'))
-      expect(repository.description).to eq('A repo with some really good code.')
-      expect(repository.link).to eq('https://github.com/github/code')
-      expect(repository.owner).to eq('github')
-      expect(repository.title).to eq('code')
-    end
+        expect(repository).to be_a(Repository)
+      end
 
-    it 'serializes pull requests' do
-      pull_request = data.first
-      allow(PullRequestSerializer).to receive(:perform).and_call_original
+      it 'fills the repository with attributes' do
+        repository = RepositorySerializer.perform(repository_data)
 
-      repository = RepositorySerializer.perform(data)
+        expect(repository.created_at).to eq(Time.parse('2011-05-09T22:53:13Z'))
+        expect(repository.description).to eq('A repo with some really good code.')
+        expect(repository.link).to eq('https://github.com/github/code')
+        expect(repository.owner).to eq('github')
+        expect(repository.title).to eq('code')
+      end
 
-      expect(PullRequestSerializer).to have_received(:perform).with(pull_request)
-      expect(repository.pull_requests.first).to be_a(PullRequest)
+      it 'does not have pull requests' do
+        repository = RepositorySerializer.perform(repository_data)
+
+        expect(repository.pull_requests).to be_empty
+      end
     end
   end
 
-  def data
-    @_data ||= JSON.parse(fixture_load('github', 'pulls.json'))
+  def pulls
+    @_pulls ||= JSON.parse(fixture_load('github', 'pulls.json'))
+  end
+
+  def repository_data
+    @_repo ||= JSON.parse(fixture_load('github', 'repository.json'))
   end
 end
