1f253f42f3e1b30793204241ff4dd1b25d0e078b
Edward Loveall <edward@edwardloveall.com>
2015-12-29 14:00:14 -0500

    Check for Keen ENV variable instead of requiring it
    This was causing the app to crash in development or other environments

--- app/services/analytics.rb
+++ app/services/analytics.rb
@@ -1,6 +1,6 @@
 module Analytics
   def requested_feed(owner:, repo:)
-    if ENV.fetch('KEEN_WRITE_KEY')
+    if ENV['KEEN_WRITE_KEY'].present?
       KeenAnalytics.requested_feed(owner: owner, repo: repo)
     end
   end
