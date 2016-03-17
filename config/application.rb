a44538cc1ef40c3cf90c1c70069c83c77dee826f
Edward Loveall <edward@edwardloveall.com>
2016-03-17 18:19:16 -0400

    Add active record

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
+require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
@@ -42,6 +43,8 @@ module PullFeed
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
     config.active_job.queue_adapter = :delayed_job
   end
 end
