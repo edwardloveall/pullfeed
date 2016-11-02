2bb24c129f9bb8c39c471ca090907f91f7c5ea75
Edward Loveall <edward@edwardloveall.com>
2016-11-02 11:05:58 -0400

    Remove deprecations from production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.static_cache_control = "public, max-age=#{1.year.to_i}"
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{1.year.to_i}" }
   # Enable deflate / gzip compression of controller-generated responses
   config.middleware.use Rack::Deflater
 
