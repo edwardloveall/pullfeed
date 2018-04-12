772655e1e16ae8916a78b206f1c2fc79ca1a2ff6
Edward Loveall <edward@edwardloveall.com>
2018-04-12 13:52:44 -0400

    Remove active job
    On the server, I was getting a bunch of "cannot load such file -- delayed_job" errors. I'm not using any kind of queue/job system, so I don't think I need active job. I suppose we'll find out, but I'm trying to remove it for now.

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
@@ -41,7 +40,5 @@ module PullFeed
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    config.active_job.queue_adapter = :delayed_job
   end
 end
